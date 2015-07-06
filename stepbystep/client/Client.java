0ed4330bea488398eb187880825669f22118b837
Zhaoyu Wei <weixiaobo88@gmail.com>
2015-07-06 18:21:09 +0800

    zy: rebuild step2

--- stepbystep/client/Client.java
+++ stepbystep/client/Client.java
@@ -1,10 +1,14 @@
 package client;
 
 import service.Service;
+import service2.Service2;
 
 public class Client {
   public static void main(String[] args) {
     Service server = new Service();
     server.service();
+
+    Service2 server2 = new Service2();
+    server2.service();
   }
 }
---
+++ stepbystep/service22/Service2.java
@@ -0,0 +1,7 @@
+package service2;
+
+public class Service2 {
+    public void service() {
+        System.out.println("Hello World 2");
+    }
+}
